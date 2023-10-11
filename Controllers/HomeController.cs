13cdf3afca4709b34ed656c6f209eb85023da7e3
gurman2912 <w0830963@myscc.ca>
2023-10-11 13:11:54 -0400

    add about page and got error but it solved after lots of efforts.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FlowerBeauty.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
