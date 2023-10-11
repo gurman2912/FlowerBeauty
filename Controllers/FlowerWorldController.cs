253cad54b6663150e28210f44bb82b80f061cfa4
gurman2912 <w0830963@myscc.ca>
2023-10-11 13:24:08 -0400

    part-2 add controller and make changes and tested.

---
+++ Controllers/FlowerWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace FlowerBeauty.Controllers
+{
+    public class FlowerWorldController : Controller
+    {
+        //GET: /FlowerWorld
+        public string Index()
+        {
+            return "This is my default action....";
+        }
+    }
+}
