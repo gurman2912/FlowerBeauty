30b5acb2b29a819308020ff8a9912d128f148b57
gurman2912 <w0830963@myscc.ca>
2023-10-11 15:09:46 -0400

    part4- create model and add coding for product attributes.

---
+++ Models/Flower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlowerBeauty.Models
+{
+    //part4- create model and attributes for flower products
+    public class Flower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string StoreLocation {get; set;}
+        public string Rating { get; set; }
+    }
+}
