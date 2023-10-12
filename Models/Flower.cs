af57242bb7a7efe2a6a40a3af1664220d69f0bb1
gurman2912 <w0830963@myscc.ca>
2023-10-12 00:29:56 -0400

    part-6 add data annotations

--- Models/Flower.cs
+++ Models/Flower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FlowerBeauty.Models
 {
@@ -9,10 +10,13 @@ namespace FlowerBeauty.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [Display(Name = "Release Date")] //part6 -add code to covert ReleaseDate as Release data
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]  //add data annotations
         public decimal Price { get; set; }
         public string StoreLocation {get; set;}
         public string Rating { get; set; }
