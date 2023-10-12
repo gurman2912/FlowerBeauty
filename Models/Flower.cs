813fad8002732b51110b65c55432e2c27460752f
gurman2912 <w0830963@myscc.ca>
2023-10-12 12:43:08 -0400

    part9- apply regular expression and validation rules on atrributes

--- Models/Flower.cs
+++ Models/Flower.cs
@@ -8,17 +8,57 @@ namespace FlowerBeauty.Models
     public class Flower
     {
         public int Id { get; set; }
+
+
+        //part9- apply regular expression and validation rules
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
 
-        [Display(Name = "Release Date")] //part6 -add code to covert ReleaseDate as Release data
+
+
+        [Display(Name = "Launch Date")] //part6 -add code to covert ReleaseDate as Release data
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Category { get; set; }
+
+
+
+
+        [RegularExpression(@"^[A-Za-z0-9\s.,'""()! ? &/ -]+$")]
+        [Required]
+        [StringLength(400)]
         public string Description { get; set; }
 
+
+
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]  //add data annotations
         public decimal Price { get; set; }
+
+
+
+
+        [Display(Name = "Store Location")]
+        [RegularExpression(@"^[A-Za-z0-9\s.,'""()! ? &/ -]+$")]
+        [Required]
+        [StringLength(30)]
         public string StoreLocation {get; set;}
+
+
+
+        
+        [RegularExpression(@"^(\d+(\.\d{0,1})?|10(\.0{0,1})?)$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
